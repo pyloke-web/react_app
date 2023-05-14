f1de89b53c6468c08fd1bbbd9f589529470ca57c
Loke <pakyenloke@gmail.com>
2023-05-14 21:29:46 +0800

    routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,8 @@ const App = () => {
     <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route index element={<Home/>}/>
+        <Route path='' element={<Home/>}/>
+        <Route path='react_app' element={<Home/>}/>
         <Route path='about' element={<About/>}/>
         <Route path='contact' element={<Contact/>}/>
         <Route path='gallery' element={<Gallery/>}/>
